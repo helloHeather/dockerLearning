f233b00c2d75f459cda2e33c2ba02c399f503dd2
Heather Danielson <sayhello.heather@gmail.com>
2018-05-08 11:31:48 -0500

    add list method

--- start/StartVM.java
+++ start/StartVM.java
@@ -6,6 +6,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+import java.security.NoSuchAlgorithmException;
+import java.security.KeyManagementException;
+
 //import org.json.simple.JSONObject; //json-simple-1.1.1.jar
 
 /*
@@ -27,7 +37,7 @@ public class StartVM {
 		StartVM http = new StartVM();
 
 		 // Sending get request
-		//http.sendingGetRequest();
+		http.sendingPostListRequest("/vms/list");
 
 		// Sending post request
 		http.sendingPostRequest("/vms");
@@ -35,11 +45,13 @@ public class StartVM {
 	 }
 	 
 	 // HTTP GET request
-	 public void sendingGetRequest(String url) throws Exception {
+	 public void sendingGetRequest(String url) throws Exception{
 	  
+
+
 		URL obj = new URL(prismCentralIP + apiPrefix + url);
 
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
 		// By default it is GET request
 		con.setRequestMethod("GET");
@@ -69,12 +81,14 @@ public class StartVM {
 	 }
 	 
 	 // HTTP Post request
-	 public void sendingPostRequest(String url) throws Exception {
+	 public void sendingPostRequest(String url) throws Exception{
 	 
 		URL obj = new URL(prismCentralIP + apiPrefix + url);
 
+		setCert();
+
 		System.out.println("sendingPostRequest: " + obj.toString());
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
 		    // Setting basic post request
 		con.setRequestMethod("POST");
@@ -108,12 +122,84 @@ public class StartVM {
 		System.out.println(response.toString());
 	}
 
+	// HTTP Post request
+	 public void sendingPostListRequest(String url) throws Exception{
+	 
+		URL obj = new URL(prismCentralIP + apiPrefix + url);
 
-	public String createJSONObject() {
+		setCert();
+
+		System.out.println("sendingPostRequest: " + obj.toString());
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		    // Setting basic post request
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		con.setRequestProperty("Content-Type","application/json");
+		con.setRequestProperty  ("Authorization", "Basic " + encoding);
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes("{'kind': 'vm', 'sort_order': 'ASCENDING', 'offset': 0, 'length': 10, 'sort_attribute': 'name'}");
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("nSending 'POST' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		      new InputStreamReader(con.getInputStream()));
+		String output;
+		StringBuffer response = new StringBuffer();
+
+		while ((output = in.readLine()) != null) {
+			response.append(output);
+		}
+		in.close();
+
+		//printing result from response
+		System.out.println(response.toString());
+	}
+
+
+
+	private String createJSONObject() {
 		String jsonString = 
-			"'spec': {'cluster_reference': {'kind': 'cluster', 'name': 'POC074', 'uuid': '00056b91-2968-462b-0000-00000000e606'}, 'description': 'My VM', 'resources': {'vnuma_config': {'num_vnuma_nodes': 0 }, 'nic_list': [], 'num_vcpus_per_socket': 1, 'num_sockets': 1, 'gpu_list': [], 'memory_size_mib': 1024, 'power_state': 'ON', 'hardware_clock_timezone': 'UTC', 'power_state_mechanism': {'mechanism': 'HARD'}, 'vga_console_enabled': true, 'disk_list': [] }, 'name': 'Test VM'}, 'api_version': '3.1', 'metadata': {'last_update_time': '2018-05-08T15:22:14Z', 'kind': 'vm', 'name': 'My VM', 'project_reference': {'kind': 'project', 'name': 'default', 'uuid': '10a25e69-007a-48fd-860b-08fa09a98c41'}, 'creation_time': '2018-05-08T15:15:34Z', 'spec_version': 1, 'owner_reference': {'kind': 'user', 'uuid': '00000000-0000-0000-0000-000000000000', 'name': 'admin'}, 'categories': {}, 'uuid': 'e7432835-9f81-4b02-beed-e99220aa3638'} 'spec': {'cluster_reference': {'kind': 'cluster', 'name': 'POC074', 'uuid': '00056b91-2968-462b-0000-00000000e606'}, 'description': 'test-vm-clone', 'resources': {'vnuma_config': {'num_vnuma_nodes': 0 }, 'nic_list': [{'nic_type': 'NORMAL_NIC', 'subnet_reference': {'kind': 'subnet', 'name': 'Primary', 'uuid': 'db2b8daa-7fd7-462b-b249-904be4240106'} } ], 'num_vcpus_per_socket': 1, 'num_sockets': 2, 'gpu_list': [], 'memory_size_mib': 4096, 'power_state': 'OFF', 'hardware_clock_timezone': 'Asia/Calcutta', 'power_state_mechanism': {'mechanism': 'HARD'}, }, 'name': 'Test VM'}, 'api_version': '3.1', 'metadata': {'kind': 'vm', 'spec_version': 0, 'uuid': 'faabffba-8234-49d5-9759-c811e90dc71b', 'categories': {} }";
-	
+			  "{'spec': {'cluster_reference': {'kind': 'cluster', 'name': 'POC074', 'uuid': '00056b91-2968-462b-0000-00000000e606'}, 'description': 'test-vm-nic', 'resources': {'vnuma_config': {'num_vnuma_nodes': 0 }, 'nic_list': [{'nic_type': 'NORMAL_NIC', 'subnet_reference': {'kind': 'subnet', 'name': 'Primary', 'uuid': 'db2b8daa-7fd7-462b-b249-904be4240106'} }], 'num_vcpus_per_socket': 1, 'num_sockets': 2, 'gpu_list': [], 'memory_size_mib': 4096, 'power_state': 'OFF', 'hardware_clock_timezone': 'Asia/Calcutta', 'power_state_mechanism': {'mechanism': 'HARD'}, 'vga_console_enabled': true }, 'name': 'Prateek Test VM'}, 'api_version': '3.1', 'metadata': {'kind': 'vm', 'last_update_time': '2018-05-08T15:22:14Z', 'spec_version': 0, 'creation_time': '2018-05-08T15:15:34Z', 'owner_reference': {'kind': 'user', 'uuid': '00000000-0000-0000-0000-000000000000', 'name': 'admin'} } }";
 		return jsonString;
+
+	}
+
+	private void setCert() throws NoSuchAlgorithmException, KeyManagementException {
+		// Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+ 
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+ 
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+ 
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
 
 	}
 
